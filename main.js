82323b77e519aef3a0b85e802f3eef3e77da0ef1
shoeiedibistl <127901044+shoeiedibistl@users.noreply.github.com>
2023-09-16 21:00:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const myVideo = document.getElementById("myVideo");
+const myTimer = document.getElementById("video__timer");
+let m
+let s
+let ms
+
+myVideo.addEventListener("click", () => (myVideo.paused)? myVideo.play() : myVideo.pause());
+
+myVideo.addEventListener('ended', () => myVideo.currentTime = 0, false);
+
+myVideo.addEventListener('timeupdate', function () {
+    myTimer.innerHTML = timeTransform(myVideo.currentTime);
+}, false);
+
+function timeTransform(time){
+
+    m = Math.floor(time / 60);
+    s = Math.floor(time % 60);
+    ms = Math.ceil((time % 1)*1000);
+
+    (m < 10) ? m = "0" + m : m;
+    (s < 10) ? s = "0" + s : s;
+    (("" + ms).length === 3) ? ms : (("" + ms).length === 2) ? ms = "0" + ms : ms = "00" + ms;
+
+    return m + ":" + s + ":" + ms;
+}
